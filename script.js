a4d5c8890291bc6c30f74c426c3d3d49461a07ba
the42ndturtle <the42ndturtle@gmail.com>
2018-04-28 09:24:46 -0400

    weight sound theme

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 class drop{
-  constructor(x, y, lifespan){
+  constructor(x, y, lifespan, weight){
     this.x = x;
     this.y = y;
     this.lifespan = lifespan;
+    this.weight = weight;
     this.life = 0;
     this.alive = true;
+    let rand = Math.floor(random(3));
+    if(rand ==  0) this.sound = new Audio('sound/high.mp3');
+    else if(rand == 1) this.sound = new Audio('sound/medium.mp3');
+    else if(rand == 2) this.sound = new Audio('sound/low.mp3');
+    if(tracker.sound) this.sound.play()
   }
   handle(){
+    // if(this.life == 1) this.sound.play();
     if(this.life <= this.lifespan){
       let opacity = map(this.life, this.lifespan, 0, 0, 1);
-      stroke(`rgba(0, 0, 0, ${opacity})`);
+      if(tracker.theme == "pond") stroke(`rgba(0, 106, 255, ${opacity})`);
+      else if(tracker.theme == "lava") stroke(`rgba(221, 82, 2, ${opacity})`);
+      else if(tracker.theme == "black and white") stroke(`rgba(0, 0, 0, ${opacity})`);
       ellipse(this.x, this.y, 5 * this.life);
       this.life++;
     }else this.alive = false;
@@ -20,6 +29,9 @@ class Tracker{
     this.frequency = 50;
     this.minLifespan = 20;
     this.maxLifespan = 120;
+    this.weight = 1;
+    this.sound = false;
+    this.theme = "pond";
   }
 }
 tracker = new Tracker
@@ -32,18 +44,21 @@ function setup() {
 }
 
 function draw(){
-  background(255, 255, 255);
+  if(tracker.theme == 'pond') background(66, 167, 244);
+  else if(tracker.theme == 'lava') background(255, 140, 73);
+  else if(tracker.theme == 'black and white') background(255, 255, 255);
   for(let i=drops.length-1; i>=0; i--){
+    strokeWeight(drops[i].weight);
     drops[i].handle();
     if(!drops[i].alive) drops.splice(i, 1)
   }
   if(tracker.frequency != 0 && Math.floor(random(0, 100 - tracker.frequency)) == 0){
-    drops.push(new drop(random(0, windowWidth), random(0, windowHeight), Math.floor(random(tracker.minLifespan, tracker.maxLifespan))));
+    drops.push(new drop(random(0, windowWidth), random(0, windowHeight), Math.floor(random(tracker.minLifespan, tracker.maxLifespan)), Math.floor(random(1, tracker.weight + 1))));
   }
 }
 
 function mouseClicked(){
-  drops.push(new drop(mouseX, mouseY, Math.floor(random(tracker.minLifespan, tracker.maxLifespan))));
+  drops.push(new drop(mouseX, mouseY, Math.floor(random(tracker.minLifespan, tracker.maxLifespan)), Math.floor(random(1, tracker.weight + 1))));
 }
 
 $(document).ready(function(){
@@ -55,9 +70,14 @@ $(document).ready(function(){
   const minLifespanText = $('#minLifespanText');
   const maxLifespanSlider = $('#maxLifespanSlider');
   const maxLifespanText = $('#maxLifespanText');
+  const weightSlider = $('#weightSlider');
+  const weightText = $('#weightText');
+  const soundToggle = $('#sound');
+  const themeSelector = $('#themeSelector');
   frequencySlider.val(tracker.frequency);
   minLifespanSlider.val(tracker.minLifespan);
   maxLifespanSlider.val(tracker.maxLifespan);
+  weightSlider.val(tracker.weight);
   gear.click(function(){
     if(settingsDiv.css('display') == 'none'){
       settingsDiv.css({'display': 'block'});
@@ -80,5 +100,22 @@ $(document).ready(function(){
   maxLifespanSlider.change(function(){
     tracker.maxLifespan = $(this).val();
     maxLifespanText.text(tracker.maxLifespan);
+  });
+  weightSlider.change(function(){
+    tracker.weight = $(this).val();
+    weightText.text(tracker.weight);
+  });
+  soundToggle.click(function(){
+    $(this).toggleClass('sounding');
+    if($(this).hasClass('sounding')){
+      soundToggle.attr('src', 'images/soundOn.png');
+      tracker.sound = true;
+    }else{
+      soundToggle.attr('src', 'images/soundOff.png');
+      tracker.sound = false;
+    }
+  });
+  themeSelector.change(function(){
+    tracker.theme = $(this).val();
   });
 });
