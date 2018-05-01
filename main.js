a2d78b05bd1e8f866a2fc962dcc8740d75849ee4
the42ndturtle <the42ndturtle@gmail.com>
2018-05-01 15:24:13 -0400

    electron again

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({width: 1000, height: 800, "node-integration": false});
   // and load the index.html of the app.
-  win.loadURL('http://localhost/rain');
+  win.loadURL('http://rain.42turtle.com');
 }
 
 app.on('ready', createWindow)
